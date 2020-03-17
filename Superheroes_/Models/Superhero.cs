a9b867a2c65580f8a4b5a348742a4a77d7e091d7
jrapkin <jrap1243@gmail.com>
2020-03-16 19:56:57 -0500

    Added Superhero model with Id primary key, and properties: Name, AlterEgo, SuperPower, SecondaryPower, catchphrase.

---
+++ Superheroes_/Models/Superhero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superheroes_.Models
+{
+	public class Superhero
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string AlterEgo { get; set; }
+		public string SuperPower { get; set; }
+		public string SecondaryPower { get; set; }
+		public string Catchphrase { get; set; }
+	}
+}
